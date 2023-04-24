0dc54a64c65ec21f099387c5828c0bb77a088610
luke <s05lbaum@uni-bonn.de>
2023-04-24 15:38:21 +0200

    namepsace angepasst

--- include/HashendeMenge.hpp
+++ include/HashendeMenge.hpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
-namespace ProjektAlpha
+namespace ProjectAlpha
 {
     template <class K>
     class HashendeMenge
     {
     public:
-        virtual HashendeMenge() = 0;
+        HashendeMenge() = 0;
         virtual void insert(K) = 0;
         virtual bool find(K) = 0;
         virtual void remove(K) = 0;
+        //Belegungsfaktor wird errechnet, um die Buckets gleichmßig zu füllen
         virtual void belegungsfaktor(K) = 0;
     };
 }
