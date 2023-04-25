8925dfd5bd220740e5cf8029606189672ce9a057
Aicha <s28aerra@uni-bonn.de>
2023-04-25 16:13:31 +0200

    Hab die Funktion get_first, die den ersten Knoten der Liste zurueckgibt hinzugefuegt

---
+++ src/Double_Linked_List.cpp
@@ -0,0 +1,26 @@
+#include "Doubly_Linked_List.hpp"
+#include "Doubly_Linked_Listnode.hpp"  
+#include "iostream"  
+
+namespace ProjectAlpha{
+    //template <class T>
+   /* creating a new node with given data and nullptr to next and prev node
+    std::shared_ptr<DoublyLinkedListNode<T>>DoublyLinkedList<T>::DoublyLinkedListNode (T data){
+        data_ = data;
+    std::shared_ptr<DoublyLinkedListNode<T>> next = nullptr;
+    std::weak_ptr<DoublyLinkedListNode<T>> prev;
+    }*/
+
+   /* template <class T>
+    // initializing head and tail (as nullptr)
+    std::shared_ptr<DoublyLinkedListNode<T>>DoublyLinkedList<T>::DoublyLinkedList(){
+        head = nullptr;
+        tail = nullptr;
+    }*/
+    template <class T>
+    // returns first node of the current list (head)
+    std::shared_ptr<DoublyLinkedListNode<T>>DoublyLinkedList<T>::get_first() const{
+        return head;
+    }
+
+}
