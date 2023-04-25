22edb7f8dec525850c64088178a42eed1ac9fbd8
Imene <benamerbelkacemi0@gitlab.informatik.uni-bonn.de>
2023-04-25 21:39:12 +0200

    angepasst wegen änderungen

--- examples/afg1.cpp
+++ examples/afg1.cpp
@@ -11,14 +11,14 @@ using namespace ProjectAlpha;
 
 int main(){
     stack<int> s;
-    s.add(5);
-    s.add(7);
-    s.add(16);
+    s.push(5);
+    s.push(7);
+    s.push(16);
     s.stackDisplay();
-    s.add(33);
-    s.add(9);
+    s.push(33);
+    s.push(9);
     s.stackDisplay();
-    s.pop();
+    s.remove();
     
     std::cout<<"Size of list:" << s.getsize() <<std::endl;
     std::cout<<"first element: " << s.peek() <<std::endl;
