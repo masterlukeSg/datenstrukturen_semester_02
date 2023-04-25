ca606ca509578d4295945eb000681cee2df53ada
luke <s05lbaum@uni-bonn.de>
2023-04-25 14:28:46 +0200

    Insert Funktion wurde programmiert

--- src/HashendeMengeRealisation.cpp
+++ src/HashendeMengeRealisation.cpp
@@ -3,13 +3,26 @@
 #include <string>
 #include "../include/list.hpp"
 
-HashendeMengeRealisation::HashendeMengeRealisation(): hashfkt(std::hash<std::string>()), num_buckets(32)
+HashendeMengeRealisation::HashendeMengeRealisation() : hashfkt(std::hash<std::string>()), num_buckets(32)
 {
-     buckets = std::vector<List<std::string>>(num_buckets);
+    buckets = std::vector<List<std::string>>(num_buckets);
 }
 void HashendeMengeRealisation::insert(std::string s)
 {
-    
+    if (find(s))
+        return;
+
+    double sizeD = size();
+    double num_bucketsD = num_buckets;
+
+    if ((sizeD / num_bucketsD) < 0.75)
+        buckets[hashfkt(s) % num_buckets].insert_front(s);
+
+    else
+    {
+        belegungsfaktor();
+        insert(s);
+    }
 }
 
 bool HashendeMengeRealisation::find(std::string s)
@@ -24,6 +37,6 @@ void HashendeMengeRealisation::remove(std::string s)
     // none
 }
 
-void HashendeMengeRealisation::belegungsfaktor(std::string s)
+void HashendeMengeRealisation::belegungsfaktor()
 {
 }
