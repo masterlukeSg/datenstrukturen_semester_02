e678b7230b25a969dd2590e984ed178e20f6bbbf
Aicha <s28aerra@uni-bonn.de>
2023-04-25 16:18:16 +0200

    Hab die Funktion insert_front hinzugefuegt, die vorne einfuegt

--- src/Double_Linked_List.cpp
+++ src/Double_Linked_List.cpp
@@ -27,5 +27,23 @@ template <class T>
     std::shared_ptr<DoublyLinkedListNode<T>>DoublyLinkedList<T>::get_last() const{
         return tail;
     }
+    
+    template <class T>
+    // inserting given integer at the front of the list
+    std::shared_ptr<DoublyLinkedListNode<T>>DoublyLinkedList<T>::insert_front(T x) {
+        std::shared_ptr<DoublyLinkedListNode<T>> new_node = std::make_shared<DoublyLinkedListNode<T>>(x); // creating node
+        new_node->next = head; // changing next pointer to the current head
+        // if there is a tail then the previous pointer of the current head is the new head
+        if (tail){
+            head->prev = new_node;
+        }
+        // if there is no tail then the new head is the tail
+        else {
+            tail = new_node;
+        }
+        head = new_node; // new head is new node
+
+        return head;
     }
 
+}
