55a0877df4646e89c727f51207432b1aec2e1913
Imene <benamerbelkacemi0@gitlab.informatik.uni-bonn.de>
2023-04-25 09:03:01 +0200

    isempty() und peek() hinzugefügt

--- include/stacktemplate.hpp
+++ include/stacktemplate.hpp
@@ -1,4 +1,6 @@
+#pragma once
 #include <iostream>
+
 
 
 namespace ProjectAlpha{
@@ -7,12 +9,15 @@ namespace ProjectAlpha{
     template <class T>
 
     class stacktemplate {
+
         public:
 
         
         virtual int size()=0;
         virtual void add(T)=0;
         virtual auto pop()=0;
+        virtual auto peek()=0;
+        virtual bool isempty()=0;
 
     };
 
