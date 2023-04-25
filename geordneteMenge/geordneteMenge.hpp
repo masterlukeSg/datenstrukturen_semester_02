f138dc33698cfd9bec4150756cabefdd00aee5e2
luke <s05lbaum@uni-bonn.de>
2023-04-25 12:41:39 +0200

    Interface fertiggstellt

---
+++ geordneteMenge/geordneteMenge.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+namespace ProjectAlpha
+{
+
+    template <class X>
+    class gerodneteMenge
+    {
+        virtual void insert(K) = 0;
+        virtual bool find(K) = 0;
+        virtual remove(K) = 0;
+
+        
+
+    };
+
+}
