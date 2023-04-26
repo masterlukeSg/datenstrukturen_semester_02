dfb1a82e59467ae40c10d720baf49b453d1384ff
Aicha <s28aerra@uni-bonn.de>
2023-04-26 13:33:40 +0200

    Hab was in der hpp fuer Listnode verbessert

--- include/listnode.hpp
+++ include/listnode.hpp
@@ -2,11 +2,45 @@
 #include <memory>
 
 
+
 namespace ProjectAlpha {
 
 template <class T>
 class ListNode{
+  public:
+  ListNode(T data);
+
+// Für ein nachvollziehbares Projekt sollte der folgende, schlechte Stil (!)
+// nicht verwendet werden. Der schlechte Stil (Membervariablen sind public) wird
+// hier nur deshalb verwendet, um die Datenstruktur schnell zu entwickeln.
 public:
+  std::shared_ptr<ListNode> next;
+  
+  T data_;
+};
+
+// In the end, the ListNodes are dynamically organized on the heap.
+// Therefore, we use Shared Pointer for the organization of ListNodes.
+// It will turn out, that shared_ptr is the best way because we want
+// to iterate through the nodes (i.e., we want multiple references at once).
+
+// The type "std::shared_ptr<ListNode>" is abbriviated with "ListNodeptr"
+
+ typedef std::shared_ptr<ListNode<class T>> ListNodeptr;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*public:
   ListNode(T data) : data_(data), next(nullptr) {}
 
 
@@ -18,4 +52,4 @@ public:
 
 
 
-}
+}*/
