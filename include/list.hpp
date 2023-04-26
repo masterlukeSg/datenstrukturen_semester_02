a39a6cada08379eba8ce7d05cb1916cf6187b817
Aicha <s28aerra@uni-bonn.de>
2023-04-26 13:31:56 +0200

    Hab was in der hpp von Liste verbessert

--- include/list.hpp
+++ include/list.hpp
@@ -1,8 +1,41 @@
 #pragma once
 #include <memory>
-#include "../include/listnode.hpp"
+#include "listnode.hpp"
+#include "E_Liste_Template.hpp"
 
-namespace ProjectAlpha {
+
+namespace ProjectAlpha{
+
+    template<class T>
+    class List{
+
+        public:
+        List();
+
+      // Insert and delete Nodes
+     ListNodeptr insert_front(T x);
+     ListNodeptr insert_after(const ListNodeptr& current, T x);
+     ListNodeptr remove_front();
+     ListNodeptr remove_after(const ListNodeptr& current);
+
+     // Get the next Node of the current List
+     ListNodeptr next(const ListNodeptr& current);
+
+     // Print the current List
+     void print() const;
+
+     private:
+     ListNodeptr head;
+};
+
+}
+
+    
+
+
+
+
+/*namespace ProjectAlpha {
 
 template <class T> 
 class List{
@@ -26,3 +59,4 @@ class List{
 };
 
 }
+*/
