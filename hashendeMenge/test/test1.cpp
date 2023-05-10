2af538dd3f41bd057775e2eb9e2169f9d524f4d0
luke <s05lbaum@uni-bonn.de>
2023-05-10 20:55:52 +0200

    Hashende Menge Insert,Remove fkt test geschrieben

--- hashendeMenge/test/test1.cpp
+++ hashendeMenge/test/test1.cpp
@@ -23,6 +23,30 @@ TEST(hashendeMengerealisation, size)
 
     n.insert(3);
     n.insert(2);
+    
+    n.remove(2);
 
-    EXPECT_EQ(n.size(), 2) << "Die Size der HashendeMenge ist nicht richtig";
+    EXPECT_EQ(n.size(), 1) << "Die Size der HashendeMenge ist nicht richtig";
+}
+
+
+TEST(hashendeMengerealisation, insert)
+{
+    hashendeMengerealisation<int> n;
+
+    n.insert(3);
+    n.insert(2);
+
+    EXPECT_EQ(n.size(), 2) << "Die Insertfunktion der HashendeMenge funktioniert nicht";
+}
+
+TEST(hashendeMengerealisation, remove)
+{
+    hashendeMengerealisation<int> n;
+    for (int i = 0; i < 10; i++)
+        n.insert(i);
+    int x = 6;
+    n.remove(x);
+    EXPECT_EQ(n.find(6), false) << "Das Element " << x << " wurde nicht entfert";
+
 }
